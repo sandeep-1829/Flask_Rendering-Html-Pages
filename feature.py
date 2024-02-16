8ecf07f50acc3b71e7e57200f090d70365e1736a
sandeep-1829 <godugunurusandeep@gmail.com>
2024-02-16 19:40:37 +0530

    first commit

---
+++ feature.py
@@ -0,0 +1,19 @@
+from flask import Flask,render_template
+
+FAI=Flask(__name__)
+
+
+
+@FAI.route('/send_html')
+def send_html():
+    return render_template('send_html.html',name='Rohit-45')
+
+
+
+@FAI.route('/properties')
+def properties():
+    return render_template('properties.html')
+
+
+if __name__=='__main__':
+    FAI.run(debug=True)
